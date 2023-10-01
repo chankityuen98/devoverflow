4ee01ae8410f25e05ba314c8331fd35c8cfc792a
Chan Kit Yuen <chankityuen98@gmail.com>
2023-10-01 16:16:28 +0800

    Added meta data and assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,29 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import React from 'react' 
-export const metadata = {
-  title: 'Next.js 13 with Clerk',
+// eslint-disable-next-line camelcase
+import {Inter, Space_Grotesk} from 'next/font/google'
+import type { Metadata } from 'next'
+
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100','200','300','400','500','600','700','800','900'],
+  variable: '--font-inter'
+})
+
+const spaceGrotest = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300','400','500','600','700' ],
+  variable: '--font-inter'
+})
+
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description: "Clone of StackOverFlow",
+  icons:{
+    icon: "/public/assets/images/site-logo.svg"
+  }
 }
  
 export default function RootLayout({
@@ -10,9 +32,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider 
+    appearance={{
+      elements:{
+        formButtonPrimary: 'primary-gradient',
+        footerActionLink: "primary-text-gradient hover:text-primary-500"
+      }
+    }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotest.variable}`}>
+
+          {children}
+          </body>
       </html>
     </ClerkProvider>
   )
