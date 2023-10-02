a528b3c085557f05de072ca930bb4cd57c8140a6
Chan Kit Yuen <chankityuen98@gmail.com>
2023-10-02 21:34:43 +0800

    Implement Theme Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,53 @@
 import { ClerkProvider } from '@clerk/nextjs'
-import React from 'react' 
+import React from 'react'
 // eslint-disable-next-line camelcase
-import {Inter, Space_Grotesk} from 'next/font/google'
+import { Inter, Space_Grotesk } from 'next/font/google'
 import type { Metadata } from 'next'
 
 import './globals.css'
+import { ThemeProvider } from '@/context/ThemeProvider'
 
 const inter = Inter({
   subsets: ['latin'],
-  weight: ['100','200','300','400','500','600','700','800','900'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   variable: '--font-inter'
 })
 
 const spaceGrotest = Space_Grotesk({
   subsets: ['latin'],
-  weight: ['300','400','500','600','700' ],
+  weight: ['300', '400', '500', '600', '700'],
   variable: '--font-inter'
 })
 
 export const metadata: Metadata = {
   title: 'DevFlow',
   description: "Clone of StackOverFlow",
-  icons:{
+  icons: {
     icon: "/public/assets/images/site-logo.svg"
   }
 }
- 
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider 
-    appearance={{
-      elements:{
-        formButtonPrimary: 'primary-gradient',
-        footerActionLink: "primary-text-gradient hover:text-primary-500"
-      }
-    }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotest.variable}`}>
-
-          {children}
-          </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotest.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: 'primary-gradient',
+              footerActionLink: "primary-text-gradient hover:text-primary-500"
+            }
+          }}
+        >
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   )
 }
